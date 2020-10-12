f2354f9991b18ac0e3c978713084b615c1ce910c
mn-dimension <51910479+mn-dimension@users.noreply.github.com>
2020-10-12 12:52:24 +0100

    Update sendemail.rb

--- sendemail.rb
+++ sendemail.rb
@@ -1,6 +1,8 @@
 class Sendemail < Formula
   version '0.0.01'
-
+  homepage "https://github.com/mn-dimension/homebrew-brew"
+  url "https://github.com/mn-dimension/homebrew-brew"
+  
   skip_clean 'bin'
 
   def install
