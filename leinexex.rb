31faa8801c94d51d2d1fd6699ca83e8ef4b7499a
mn-dimension <thecontinium+mn.dimension@outlook.com>
2020-11-27 16:58:22 +0000

    add lein exec

---
+++ leinexex.rb
@@ -0,0 +1,15 @@
+class LeinExec < Formula
+  version '0.1.00'
+  homepage "https://github.com/mn-dimension/homebrew-brew"
+  url 'https://github.com/mn-dimension/homebrew-brew.git'
+
+  bottle :unneeded
+
+  skip_clean 'bin'
+
+  def install
+    bin.install 'lein-exec'
+    bin.install 'lein-exec-p'
+  end
+
+end
