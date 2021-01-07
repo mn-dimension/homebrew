cacda37c28e147548b80123e3aec43640c51a64c
mn-dimension <thecontinium+mn.dimension@outlook.com>
2021-01-07 15:05:58 +0000

    add dependency on coreutils

--- archivedropboxprices.rb
+++ archivedropboxprices.rb
@@ -1,10 +1,10 @@
 class Archivedropboxprices < Formula
-  version '0.1.01'
+  version '0.1.02'
   homepage "https://github.com/mn-dimension/homebrew-brew"
   url 'https://github.com/mn-dimension/homebrew-brew.git'
 
   bottle :unneeded
-
+  depends_on "coreutils"
   skip_clean 'bin'
 
   def install
