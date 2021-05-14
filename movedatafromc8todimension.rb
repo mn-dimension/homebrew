765e4370f9b8e190adf46832f38fa292e318f695
mn-dimension <thecontinium+mn.dimension@outlook.com>
2021-05-14 18:32:32 +0100

    1st cut of movedatafromc8todimension

---
+++ movedatafromc8todimension.rb
@@ -0,0 +1,14 @@
+class Movedatafromc8todimension < Formula
+version '0.0.1'
+  homepage "https://github.com/mn-dimension/homebrew-brew"
+  url 'https://github.com/mn-dimension/homebrew-brew.git'
+
+  bottle :unneeded
+  skip_clean 'bin'
+
+  def install
+    bin.install Dir["script/movedatafromc8todimension/movedatafromc8todimension"]
+    prefix.install Dir["script/movedatafromc8todimension/*.plist"]
+  end
+
+  plist_options :startup => true
