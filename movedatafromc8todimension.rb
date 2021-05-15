71011e78064bf82b7305ec89bd50929c07c371b0
mn-dimension <thecontinium+mn.dimension@outlook.com>
2021-05-15 09:30:11 +0100

    bump

--- movedatafromc8todimension.rb
+++ movedatafromc8todimension.rb
@@ -1,5 +1,5 @@
 class Movedatafromc8todimension < Formula
-version '0.0.7'
+version '0.0.8'
   homepage "https://github.com/mn-dimension/homebrew-brew"
   url 'https://github.com/mn-dimension/homebrew-brew.git'
 
