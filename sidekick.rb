c80771c5294e3906b72f59e28475e505b09709a0
mn-dimension <51910479+mn-dimension@users.noreply.github.com>
2021-11-28 11:18:06 +0000

    Update sidekick.rb

--- sidekick.rb
+++ sidekick.rb
@@ -4,7 +4,6 @@ class Sidekick < Formula
 
   version = "0.5.0"
   revision 1
-  bottle :unneeded
   depends_on :arch => :x86_64
 
   url "https://github.com/minio/sidekick/releases/download/v#{version}/sidekick_#{version}_Darwin_x86_64.tar.gz"
